8d676f7559fa00ee02cd2fda2fabb02de4270fa8
Gino Massalski <gino.massalski@gmail.com>
2022-12-26 12:45:18 +0100

    connection to server

--- src/Questionnaire/QuestionAnswers.jsx
+++ src/Questionnaire/QuestionAnswers.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 import "../App.sass";
-import { postUserData } from "../utils/postUserData";
+// import { postUserData } from "../utils/postUserData";
+import { useState } from "react";
 
 function QuestionAnswers({ userData, step }) {
-  let userDataCopy = { ...userData };
+  const [result, setResult] = useState("");
+
+  async function postUserData() {
+    const url =
+      "http://localhost:8000/getData";
+    const response = await fetch(url, {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    setResult(result)
+    // console.log(result)
+    return result;
+  }
+  
 
   return (
     <div className="answers">
       <h1>Hier die Antworten</h1>
-      <p>{userDataCopy.goal.split(",")[1]}</p>
-      <p>{userDataCopy.age.split(",")[1]}</p>
-      <p>{userDataCopy.duration.split(",")[1]}</p>
-      <p>{userDataCopy.behaviour.split(",")[1]}</p>
-      <p>{userDataCopy.selfTest}</p>
-      {userDataCopy.selfTest.length > 0 && step === 4 && (
-        <button type="button" onClick={postUserData}>Erhalte Risikoklasse</button>
+      <p>{userData.goal.split(",")[1]}</p>
+      <p>{userData.age.split(",")[1]}</p>
+      <p>{userData.duration.split(",")[1]}</p>
+      <p>{userData.behaviour.split(",")[1]}</p>
+      <p>{userData.selfTest}</p>
+      {userData.selfTest.length > 0 && step === 4 && (
+        <button type="button" onClick={() => postUserData()}>
+          Erhalte Risikoklasse
+        </button>
       )}
-      <p>{}</p>
+      <p>{result.calculatedRiskRate}</p>
     </div>
   );
 }
--- src/utils/postUserData.js
+++ src/utils/postUserData.js
@@ -1,22 +1,11 @@
-export async function postUserData() {
+export async function postUserData(setResult) {
   const url =
-    "https://sandbox.onboarding-api.evergreen.de/api/#/Risk%20Rate/RiskRateController_calcRiskRate";
-  const data = {
-    goal: "Retirement",
-    age: "Below56",
-    selfTest: 5,
-    duration: "Below5",
-    behaviour: "KeepCool",
-  };
+    "http://localhost:8000/getData";
   const response = await fetch(url, {
-    method: "POST",
-    mode: "no-cors",
-    body: JSON.stringify(data),
+    method: "GET",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
-    
   });
-  console.log(await response.json());
+  console.log(await response.json())
 }
