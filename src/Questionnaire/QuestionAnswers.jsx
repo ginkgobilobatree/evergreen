535f99b97af41349916b935fde26d7d6d2d38fbf
Gino Massalski <gino.massalski@gmail.com>
2022-12-29 07:35:26 +0100

    show result in storeData

--- src/Questionnaire/QuestionAnswers.jsx
+++ src/Questionnaire/QuestionAnswers.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { /* useState, */ useEffect } from "react";
 import "../App.sass";
 import { postUserData } from "../utils/postUserData";
 import { GeneralText, Units } from "../data/text";
 import { giveGermanText } from "../utils/giveGermanText";
 import { Link } from "react-router-dom";
 
-function QuestionAnswers({ userData, step }) {
-  const [result, setResult] = useState({});
+function QuestionAnswers({ userData, step, result, setResult }) {
+  // const [result, setResult] = useState({});
 
   useEffect(() => {
     setResult({});
